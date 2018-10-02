adfac9a4d0c2643a49312ea0ffb47f9d9a999205
sunning97 <mrcatbro97@gmail.com>
2018-10-02 08:12:51 +0700

    update GUI

--- app/src/main/java/vn/edu/ut/gts/views/dashboard/DashboardActivity.java
+++ app/src/main/java/vn/edu/ut/gts/views/dashboard/DashboardActivity.java
@@ -1,53 +1,142 @@
 package vn.edu.ut.gts.views.dashboard;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import vn.edu.ut.gts.R;
+import vn.edu.ut.gts.views.home.HomeActivity;
 
-public class DashboardActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private TextView qqq;
-    private AppBarLayout vvv;
-    private CollapsingToolbarLayout collapsingToolbarLayout;
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.dashboard_toolbar)
+    Toolbar dashboardToolbar;
+    @BindView(R.id.dashboard_student_id)
+    TextView dashboardStudentID;
+    @BindView(R.id.dashboard_appbar_layout)
+    AppBarLayout dashboardAppbarLayout;
+    @BindView(R.id.dashboard_collapsing_toolbar_alyout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.student_info_card)
+    CardView studentInfoCard;
+    @BindView(R.id.student_study_result_card)
+    CardView studentStudyResultCard;
+    @BindView(R.id.frame_program_card)
+    CardView frameProgramCard;
+    @BindView(R.id.student_debt_card)
+    CardView studentDebtCard;
+    @BindView(R.id.schedule_by_week_card)
+    CardView scheduleByWeekCard;
+    @BindView(R.id.attendance_card)
+    CardView attendanceCard;
     private String title = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        toolbar = (Toolbar) findViewById(R.id.home_toolbar);
-        vvv = findViewById(R.id.home_appbar_layout);
-        qqq = findViewById(R.id.qqq);
-        collapsingToolbarLayout = findViewById(R.id.ppp);
-        title = collapsingToolbarLayout.getTitle().toString();
-        vvv.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                if(Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
-                    qqq.setVisibility(View.INVISIBLE);
-                    collapsingToolbarLayout.setTitle(collapsingToolbarLayout.getTitle() + " - " + qqq.getText());
-                } else{
-                    qqq.setVisibility(View.VISIBLE);
-                    collapsingToolbarLayout.setTitle(title);
-                }
-            }
-        });
-
-        setSupportActionBar(toolbar);
-
+        ButterKnife.bind(this);
+        this.init();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.home_toolbar_menu, menu);
         return true;
+    }
+
+    @OnClick({
+            R.id.student_info_card,
+            R.id.student_study_result_card,
+            R.id.frame_program_card,
+            R.id.student_debt_card,
+            R.id.schedule_by_week_card,
+            R.id.attendance_card
+    })
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.student_info_card:
+                startActivity(HomeActivity.STUDENT_INFO);
+                break;
+            case R.id.student_study_result_card:
+                startActivity(HomeActivity.STUDENT_STUDY_RESULT);
+                break;
+            case R.id.frame_program_card:
+                startActivity(HomeActivity.FRAME_PROGRAM);
+                break;
+            case R.id.student_debt_card:
+                startActivity(HomeActivity.STUDENT_DEBT);
+                break;
+            case R.id.schedule_by_week_card:
+                startActivity(HomeActivity.SCHEDULE_BY_WEEK);
+                break;
+            case R.id.attendance_card:
+                startActivity(HomeActivity.ATTENDACE);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void startActivity(int order) {
+        Intent intent = new Intent(DashboardActivity.this, HomeActivity.class);
+        intent.putExtra("order", String.valueOf(order));
+        startActivity(intent);
+    }
+
+    private void init() {
+        title = collapsingToolbarLayout.getTitle().toString();
+
+        dashboardAppbarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                    dashboardStudentID.setVisibility(View.INVISIBLE);
+                    collapsingToolbarLayout.setTitle(collapsingToolbarLayout.getTitle() + " - " + dashboardStudentID.getText());
+                } else {
+                    dashboardStudentID.setVisibility(View.VISIBLE);
+                    collapsingToolbarLayout.setTitle(title);
+                }
+            }
+        });
+
+        setSupportActionBar(dashboardToolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        showExitConfirm();
+    }
+
+    private void showExitConfirm(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AlertDialogTheme);
+        builder.setTitle("Xác nhận thoát");
+        builder.setMessage("Bạn có muốn thoát ứng dụng?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Hủy bỏ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/vn/edu/ut/gts/views/home/HomeActivity.java
+++ app/src/main/java/vn/edu/ut/gts/views/home/HomeActivity.java
@@ -1,14 +1,109 @@
 package vn.edu.ut.gts.views.home;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import vn.edu.ut.gts.views.dashboard.DashboardActivity;
+import vn.edu.ut.gts.views.home.fragments.StudentInfoRootFragment;
+import vn.edu.ut.gts.views.home.fragments.StudentStudyResultFragment;
 import vn.edu.ut.gts.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    public static final int STUDENT_INFO = 1;
+    public static final int STUDENT_STUDY_RESULT = 2;
+    public static final int FRAME_PROGRAM = 3;
+    public static final int STUDENT_DEBT = 4;
+    public static final int SCHEDULE_BY_WEEK = 5;
+    public static final int ATTENDACE = 6;
+
+    @BindView(R.id.home_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.home_drawerlayout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.home_navigation_view)
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent = getIntent();
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setHomeFragment(intent);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        drawerLayout.closeDrawers();
+        switch (item.getItemId()) {
+            case R.id.student_profile:
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.home_fragment_container,
+                        new StudentInfoRootFragment()
+                ).commit();
+                setTitle(item.getTitle());
+                break;
+            case R.id.student_study_result:
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.home_fragment_container,
+                        new StudentStudyResultFragment()
+                ).commit();
+                setTitle(item.getTitle());
+                break;
+            case R.id.home_dashboard:
+                Intent dashboardIntent = new Intent(HomeActivity.this, DashboardActivity.class);
+                startActivity(dashboardIntent);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    private void setHomeFragment(Intent intent){
+        switch (Integer.parseInt(intent.getStringExtra("order"))){
+            case HomeActivity.STUDENT_INFO:
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.home_fragment_container,
+                        new StudentInfoRootFragment()
+                ).commit();
+                setTitle("Thông tin sinh viên");
+                break;
+            case HomeActivity.STUDENT_STUDY_RESULT:
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.home_fragment_container,
+                        new StudentStudyResultFragment()
+                ).commit();
+                setTitle("Kết quả học tập");
+                break;
+        }
     }
 }
