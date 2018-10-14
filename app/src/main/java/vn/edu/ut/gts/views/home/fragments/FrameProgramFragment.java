21d97f829ace8d6dfb0c7de305398908022d6d5c
sunning97 <mrcatbro97@gmail.com>
2018-10-14 20:34:40 +0700

    update

--- app/src/main/java/vn/edu/ut/gts/views/home/fragments/FrameProgramFragment.java
+++ app/src/main/java/vn/edu/ut/gts/views/home/fragments/FrameProgramFragment.java
@@ -1,6 +1,7 @@
 package vn.edu.ut.gts.views.home.fragments;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,22 +9,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import vn.edu.ut.gts.R;
+import vn.edu.ut.gts.actions.Student;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FrameProgramFragment extends Fragment {
 
+    private Student student;
 
     public FrameProgramFragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frame_program, container, false);
+        student = new Student(getContext());
+        View view = inflater.inflate(R.layout.fragment_frame_program, container, false);
+
+
+        AsyncTask<Void,Void,Void> voidVoidVoidAsyncTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                student.Test();
+                return null;
+            }
+        };
+        voidVoidVoidAsyncTask.execute();
+        return  view;
     }
 
 }
