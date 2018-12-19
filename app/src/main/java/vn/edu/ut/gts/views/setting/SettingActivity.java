747c1a63b6723d7618c3fe285d91a4108fc079bb
sunning97 <mrcatbro97@gmail.com>
2018-12-19 09:55:31 +0700

    update

--- app/src/main/java/vn/edu/ut/gts/views/setting/SettingActivity.java
+++ app/src/main/java/vn/edu/ut/gts/views/setting/SettingActivity.java
@@ -1,29 +1,125 @@
 package vn.edu.ut.gts.views.setting;
 
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.suke.widget.SwitchButton;
 
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import vn.edu.ut.gts.R;
+import vn.edu.ut.gts.helpers.Storage;
 
 public class SettingActivity extends AppCompatActivity {
     @BindView(R.id.setting_toolbar)
     Toolbar settingToolbar;
-
+    @BindView(R.id.sb_notify_week_schedule)
+    SwitchButton sbNotifyWeekChedule;
+    @BindView(R.id.setting_time_week_schedule_notify)
+    LinearLayout settingTimeWeekScheduleNotify;
+    @BindView(R.id.parent_layout)
+    LinearLayout parentLayout;
+    CharSequence[] values = {" Trước 1 giờ "," Trước 3 giờ "," Trước 5 giờ "," Trước 7 giờ "," Trước 24 giờ "};
+    AlertDialog alertDialog1;
+    TextView settingTimeWeekScheduleNotifyTextView1,settingTimeWeekScheduleNotifyTextView2;
+    private Storage storage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         ButterKnife.bind(this);
-
+        storage = new Storage(this);
+        settingTimeWeekScheduleNotifyTextView1 = (TextView) settingTimeWeekScheduleNotify.getChildAt(0);
+        settingTimeWeekScheduleNotifyTextView2 = (TextView) settingTimeWeekScheduleNotify.getChildAt(1);
+        if(storage.getString("week_schedule_notify") != null && Boolean.parseBoolean(storage.getString("week_schedule_notify"))){
+            sbNotifyWeekChedule.setChecked(true);
+            settingTimeWeekScheduleNotify.setEnabled(true);
+            settingTimeWeekScheduleNotifyTextView1.setTextColor(getResources().getColor(R.color.black));
+            settingTimeWeekScheduleNotifyTextView2.setText(storage.getString("week_schedule_notify_time"));
+        } else {
+            settingTimeWeekScheduleNotify.setEnabled(false);
+            settingTimeWeekScheduleNotifyTextView1.setTextColor(getResources().getColor(R.color.gray2));
+            settingTimeWeekScheduleNotifyTextView2.setText(values[0]);
+        }
 
         settingToolbar.setTitle("Cài đặt");
         setSupportActionBar(settingToolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        sbNotifyWeekChedule.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                settingTimeWeekScheduleNotify.setEnabled(isChecked);
+                storage.putString("week_schedule_notify",String.valueOf(isChecked));
+                if(!isChecked){
+                    settingTimeWeekScheduleNotifyTextView1.setTextColor(getResources().getColor(R.color.gray2));
+                } else settingTimeWeekScheduleNotifyTextView1.setTextColor(getResources().getColor(R.color.black));
+            }
+        });
+
+        settingTimeWeekScheduleNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAlertDialogWithRadioButtonGroup();
+            }
+        });
     }
+
+    private void CreateAlertDialogWithRadioButtonGroup(){
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+
+        builder.setTitle("Chọn thời gian thông báo");
+
+        builder.setSingleChoiceItems(values, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which)
+                {
+                    case 0:
+                        settingTimeWeekScheduleNotifyTextView2.setText(values[0]);
+                        setSettingTimeWeekScheduleNotify(0);
+                        break;
+                    case 1:
+                        settingTimeWeekScheduleNotifyTextView2.setText(values[1]);
+                        setSettingTimeWeekScheduleNotify(1);
+                        break;
+                    case 2:
+                        settingTimeWeekScheduleNotifyTextView2.setText(values[2]);
+                        setSettingTimeWeekScheduleNotify(2);
+                        break;
+                    case 3:
+                        settingTimeWeekScheduleNotifyTextView2.setText(values[3]);
+                        setSettingTimeWeekScheduleNotify(3);
+                        break;
+                    case 4:
+                        settingTimeWeekScheduleNotifyTextView2.setText(values[4]);
+                        setSettingTimeWeekScheduleNotify(4);
+                        break;
+                }
+                alertDialog1.dismiss();
+                Snackbar.make(parentLayout,"Thay đổi thành công!",Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        alertDialog1 = builder.create();
+        alertDialog1.show();
+    }
+
+    private void setSettingTimeWeekScheduleNotify(int pos){
+        storage.putString("week_schedule_notify_time",values[pos].toString());
+    }
+
 }
